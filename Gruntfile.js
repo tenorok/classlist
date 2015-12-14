75c0d8d3bafb207443870be95b7ab09f7a9aa3e9
tenorok <mail@tenorok.ru>
2015-12-14 16:38:54 +0300

    Added measurement of test coverage.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('remap-istanbul');
 
     grunt.initConfig({
         ts: {
@@ -15,9 +16,34 @@ module.exports = function(grunt) {
                 src: ['test/*.js']
             }
         },
+        mocha_istanbul: {
+            main: {
+                src: 'test/*.js',
+                options: {
+                    reaporter: 'dot',
+                    print: 'both',
+                    reportFormats: ['json']
+                }
+            }
+        },
+        remapIstanbul: {
+            main: {
+                files: [{
+                    src: 'coverage/coverage.json',
+                    dest: 'coverage/coverage.json',
+                    type: 'json'
+                }]
+            }
+        },
+        shell: {
+            istanbul: {
+                command: './node_modules/.bin/istanbul report lcov text --include coverage/coverage.json'
+            }
+        },
         clean: {
             main: [
                 '.tscache',
+                'coverage',
                 'docs'
             ].concat([
                 'test/*',
@@ -48,5 +74,6 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('test', ['ts:main', 'mochaTest:main']);
+    grunt.registerTask('coverage', ['ts:main', 'mocha_istanbul:main', 'remapIstanbul:main', 'shell:istanbul']);
 
 };
--- test/toggle.ts
+++ test/toggle.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import ClassList from '../classList';
+import ClassList from '../ClassList';
 
 describe('Method toggle.', () => {
     let classList;
